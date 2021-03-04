5ebfda0053683433a9e23a1c66624f2e698ac8ad
root <root@localhost.localdomain>
2021-03-04 11:16:20 -0500

    First commit

---
+++ dirbackup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+if [-f"$1"] then echo "$1 is a file"
+	else echo "$1 is not a file"
+fi
+
+## $1 is the first word on the command line
+## after the command itself
+
---
+++ diskconfig.sh
@@ -0,0 +1,10 @@
+
+#!/bin/bash
+if ["$1"="cool"] then echo "Cool Beans"
+	elif ["$1"="neat"] then echo "Neato Cool"
+	else echo "Not Cool Beans"
+fi
+
+## $1 if the first word on the command line
+## after the command itself
+
